53dc396dc93101085b8c61f463c4112b06cdd96c
SEMEREDIN NURGEBA <semernur11@gmail.com>
2025-09-20 07:31:10 +0300

    fix:image domain wild card and add read more functionality with blurry background for better UX.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains:  ['*.supabase.co'],
+       remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: '*.supabase.co',
+        },
+       ],
     },
 };
 
